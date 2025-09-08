e45747875489493552cf9e71134b942820eacf7e
Pedro Pablo Bustamante Barrera <ppbb15@gmaill.com>
2025-09-08 08:35:00 -0300

    chore: upgrade package version

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ test_requirements = [
 
 setuptools.setup(
     name="notbank",
-    version="2.0.3",
+    version="2.1.0",
     packages=[
         "notbank_python_sdk",
         "notbank_python_sdk.core",
